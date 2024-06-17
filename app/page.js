20e35c0178262c8f2acedd7479c3e60c2cfb245c
Shiroi <sebo1910kol@gmail.com>
2024-06-17 21:21:48 +0530

    sign-in sign-up pages done

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <UserButton/>
       <h2>subscribe yey!</h2>
     </div>
     
