7ca754819b029a0e95f6cf84be56aa95c9d3bcd1
Shiroi <sebo1910kol@gmail.com>
2024-06-17 20:22:37 +0530

    Project backend and auth initiated

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default {
     dialect: 'postgresql',
     // driver: 'pg',
     dbCredentials: {
-    url: "postgresql://neondb_owner:QObeKx5B8dsP@ep-silent-fire-a148xdvw.ap-southeast-1.aws.neon.tech/portfolio-builder?sslmode=require",
+    url: NEXT_PUBLIC_DB_URL,
     }
   };
